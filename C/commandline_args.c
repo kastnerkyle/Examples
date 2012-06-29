e8fbb544394998dd4eaa1e7a81fde59119e8fdf0
Kyle Kastner <kastnerkyle@gmail.com>
2012-06-29 10:16:41 -0500

    Fixed variable names

--- C/commandline_args.c
+++ C/commandline_args.c
@@ -1,7 +1,7 @@
 void usage()
 {
    printf("Usage is prog [options]\n");
-   printf("       -c  <c_opt> (def='none'\n");
+   printf("       -c  <c_opt> (def='Hello'\n");
    printf("       -a  <a_opt>\n");
    printf("       -d  <d_opt>\n");
    exit(1);
@@ -9,7 +9,9 @@ void usage()
 
 int main(int argc, char* argv[]) {
     int a=1;
-    char c_opt[32]="none";
+    char c_opt[32]="Hello";
+    char a_opt[32]="none";
+    char d_opt[32]="none";
     int needusage=1;
 
     while ( a < argc )
@@ -22,13 +24,13 @@ int main(int argc, char* argv[]) {
         switch(argv[a++][1])
         {
             case 'c':
-                if (argc>a) strncpy(chipadr,argv[a++],32);
+                if (argc>a) strncpy(c_opt,argv[a++],32);
                 break;
             case 'a':
-                if (argc>a) strncpy(regadr,argv[a++],32);
+                if (argc>a) strncpy(a_opt,argv[a++],32);
                 break;
             case 'd':
-                if (argc>a) strncpy(dataval,argv[a++],32);
+                if (argc>a) strncpy(d_opt,argv[a++],32);
                 needusage=0;
                 break;
         }
